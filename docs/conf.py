da1c1d973bec541be3ef7d93f23d7856e37bccfe
kartgoel <137910581+kartgoel@users.noreply.github.com>
2023-06-28 15:27:55 -0400

    Test 12

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_logo = 'hydra.png'
+html_logo = 'img/hydra.png'
 
 
 # -- Options for EPUB output
