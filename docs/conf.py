657c7f2ac164330c72f45c234fd07ad053e76f4f
kartgoel <137910581+kartgoel@users.noreply.github.com>
2023-07-10 10:39:23 -0400

    Back to original changes

--- docs/conf.py
+++ docs/conf.py
@@ -17,33 +17,21 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    "sphinx_rtd_theme",
-	'sphinx.ext.autosectionlabel',
-    'sphinx.ext.todo',
-    'sphinxext.remoteliteralinclude',
-	"sphinxext.opengraph",
-	"sphinxcontrib.ghcontributors",
-    "sphinx_design"
 ]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
+
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
-hoverxref_roles = ['term']
-
-ogp_site_url = "https://docs.hydra.org/en/latest/"
-ogp_site_name = "Hydra Documentation"
-ogp_image = "https://raw.githubusercontent.com/JeffersonLab/HydraDocumentation/blob/Development/docs/img/hydra_small_logo.png"
+html_favicon = 'assets/RoundLogo.png'
+html_title = "Hydra Docs"
 
 # -- Options for HTML output
-
-html_title = "Hydra Docs"
-html_favicon = 'img/hydra_small_logo.png'
 
 html_theme = 'sphinx_rtd_theme'
 html_logo = 'img/hydra_logo.png'
@@ -51,24 +39,8 @@ html_theme_options = {
     'collapse_navigation': False,
     'display_version': False,
     'logo-only': False,
-    'sticky-navigation': False,
-    "sidebar_hide_name": True,
-     "dark_css_variables" : {
-        "color-background-primary": "#242c37",
-        "color-background-secondary": "#006492",
-        "color-background-hover": "#efeff400",
-        "color-background-hover--transparent": "#efeff400",
-        "color-brand-primary": "#ffd843",
-        "color-brand-secondary": "#39a4d5",
-        "color-brand-content": "#ffd843",
-        "color-foreground-primary": "#ffffff",
-        "color-foreground-secondary": "#ffffff",
-        "color-foreground-muted": "#ffffff",
-        "color-foreground-border": "transparent",
-        "color-background-border": "transparent",
-        "color-api-overall": "#101010",
-        "color-inline-code-background": "#0d0d0d",
-    }
+    'sticky-navigation': False
+    "sidebar_hide_name": True
 }
 html_show_sphinx = False
 html_show_sourceLink = False
@@ -79,4 +51,3 @@ def setup(app):
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
-
